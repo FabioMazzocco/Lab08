87bd507f8a21b5d0f7e36b321e66b020a4a223f0
fabin <fabin@169.254.79.176>
2019-05-24 22:04:14 +0200

    Fatte tutte le operazioni preliminari per iniziare a fare il programma

--- src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,89 @@
 package it.polito.tdp.dizionariograph;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+
 public class DizionarioGraphController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtLettere;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private Button btnGeneraGrafo;
+
+    @FXML
+    private Button btnVicini;
+
+    @FXML
+    private Button btnGradoMax;
+
+    @FXML
+    private Button btnReset;
+    
+    @FXML
+    private TextArea txtResult;
+    
+    private Model model = null;
+    
+    @FXML
+    void doGradoMax(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void doGrafo(ActionEvent event) {
+    	int numLettere=0;
+    	try {
+    		String lettere = txtLettere.getText();
+    		numLettere = Integer.parseInt(lettere);
+    	}catch(NumberFormatException e) {
+    		txtResult
+    	}
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtLettere != null : "fx:id=\"txtLettere\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGeneraGrafo != null : "fx:id=\"btnGeneraGrafo\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnVicini != null : "fx:id=\"btnVicini\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGradoMax != null : "fx:id=\"btnGradoMax\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+    }
+
+	public Model getModel() {
+		return model;
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+    
 }
